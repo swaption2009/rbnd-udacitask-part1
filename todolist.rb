ec7a744107ffd3abbe60c16456aa3049ab7d0060
swaption2009 <swaption2009@gmail.com>
2016-06-18 21:35:34 -0700

    add more methods from rubric and 3 additional features
    rubric:
    - add_item
    - delete_item
    - change_status!
    - name_list
    - rename_list
    - print_list
    - completed?
    
    3 new features:
    - progress_report
    - sort_by_item_name
    - item_time_report

--- todolist.rb
+++ todolist.rb
@@ -7,32 +7,73 @@ class ToDoList
     @items = Array.new
   end
 
-  def add_item(new_item)
-    item = Item.new(new_item)
-    @items.push(item)
+  def add_item
+    puts "How many to-do items do you have?"
+    number_of_items = gets.chomp.to_i
+    number_of_items.times do |item|
+      puts "Enter your to-do item: "
+      to_do_item = gets.chomp.to_s
+      item = Item.new(to_do_item)
+      @items.push(item)
+    end
   end
 
-  # Feature 1
-  def rename_list(name)
+  def delete_item
+    puts "Which item number do you like to delete? (enter a task_id number): " if @items.count > 1
+    del_item = gets.chomp.to_i
+    @items.delete_at(del_item-1)
+  end
+
+  def change_status!
+    puts "Which item number have you completed? " if @items.count > 1
+    completed_item = gets.chomp.to_i
+    @items[completed_item-1].completed_status = true
+    puts "Congratulations! " + @items[completed_item].description + " is completed"
+  end
+
+  def rename_list
+    puts "Enter new name of the list: " if @items.count > 1
+    name = gets.chomp.to_s
     @title = name
+  end
+
+  def print_list
+    puts "*" * 30
+    puts @title
+    puts "*" * 30
+    @items.each { |item| puts item.description, item.completed_status ? "Status: Done" : "Status: Not Done" }
+end
+
+  # Feature 1
+  def progress_report
+    total_items = @items.count
+    completed_items = @items.select { |item| item.completed_status == true }.count
+    puts "You've completed " + completed_items.to_s + " out of " + total_items.to_s + " tasks you have."
+  end
+
+  # Feature 2
+  def sort_by_item_name
+    sorted_list = @items.sort_by! { |item| item.description }
+    sorted_list.each { |item| puts item.description }
   end
 end
 
 class Item
-  attr_accessor :description, :completed_status
+  attr_accessor :description, :completed_status, :created_at
 
   def initialize(description)
     @description = description
     @completed_status = false
+    @created_at = Time.now
   end
 
-  # Feature 2
   def completed?
     @completed_status
   end
 
   # Feature 3
-  def change_status!
-    @completed_status = !@completed_status
+  def item_time_report
+    lapse_time = Time.now - @created_at
+    puts lapse_time
   end
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,47 +1,51 @@
 require_relative 'todolist.rb'
 
-# DRY print the list
-def print_list(list)
-  puts list.title
-  list.items.each { |item| puts item.description, item.completed_status ? "Status: Done" : "Status: Not Done" }
-  puts "_" * 40
-end
-
 # Creates a new todo list
-my_list = ToDoList.new "My Ruby ToDo List"
+puts "Welcome to Udacitask! Please enter a name for your list: "
+new_name = gets.chomp.to_s
+my_list = ToDoList.new(new_name)
+puts "Thank you! You've created " + new_name + " to-do list"
 
 # Add four new items
-my_list.add_item "Complete Roobiedocks Adventure"
-my_list.add_item "Complete ToyCity Report Generator 1"
-my_list.add_item "Complete ToyCity Report Generator 2"
-my_list.add_item "Complete Udacitask 1"
+my_list.add_item
 
 # Print the list
-print_list(my_list)
+my_list.print_list
 
 # Delete the first item
-my_list.items.delete_at(0)
+my_list.delete_item
 
 # Print the list
-print_list(my_list)
+my_list.print_list
 
 # Delete the second item
-my_list.items.delete_at(0)
+my_list.delete_item
 
 # Print the list
-print_list(my_list)
+my_list.print_list
 
-# Feature 3 (update the completion status of the first item to complete)
-my_list.items[0].change_status!
-
-# Print the list
-print_list(my_list)
-
-# Feature 1(update the title of the list)
-my_list.rename_list "My New and Shiny Title"
+# Update the completion status of the first item to complete
+my_list.change_status!
 
 # Print the list
-print_list(my_list)
+my_list.print_list
 
-# Feature 2 (completed? method)
+# Update the title of the list
+my_list.rename_list
+
+# Print the list
+my_list.print_list
+
+# print completed? method
 puts "The task for " + my_list.items[0].description + " is completed? " +  my_list.items[0].completed?.to_s
+
+# Feature 1: shows how many items are completed vs total
+my_list.progress_report
+
+# Feature 2: sort ToDoList by item description
+puts "Your list sorted by description: "
+my_list.sort_by_item_name
+
+# Feature 3: calculate lapsed time for an item
+puts "This item has been outstanding for: "
+my_list.items[0].item_time_report
